63acd1383f7dfb59e4de808054f19a8070f1aee6
soporte-archivar <soporte.archivar@gmail.com>
2024-06-07 18:17:00 -0500

    conexion railway

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=punto_de_venta",
+		$link = new PDO("mysql:host=monorail.proxy.rlwy.net;dbname=railway",
 			            "root",
 			            "");
 
